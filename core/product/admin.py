789004a6383ffcbb4fddd8ad087a4f4bbf501e2b
Juan Felipe <juan.fll@hotmail.com>
2023-01-22 17:28:21 -0300

    incluindo drf-spectacular nos requirements para automação da documentação da api

--- core/product/admin.py
+++ core/product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Categoria, Marca, Produto
+
 # Register your models here.
+
+admin.site.register(Categoria)
+
+admin.site.register(Produto)
+
+admin.site.register(Marca)
