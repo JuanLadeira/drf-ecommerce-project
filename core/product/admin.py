a76bec7a9322aec7985499410b6fa37f968a3efe
Juan Felipe <juan.fll@hotmail.com>
2023-01-24 22:14:05 -0300

    adicionado ProductLineInline para mostrar subproduto junto ao produto no painel do amdmin)

--- core/product/admin.py
+++ core/product/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 
-from .models import Categoria, Marca, Produto
+from .models import Categoria, Marca, Produto, ProdutoLine
 
 # Register your models here.
 
+class ProductLineInline(admin.TabularInline):
+    model = ProdutoLine
+
+@admin.register(Produto)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline]
+
 admin.site.register(Categoria)
 
-admin.site.register(Produto)
-
 admin.site.register(Marca)
+admin.site.register(ProdutoLine)
