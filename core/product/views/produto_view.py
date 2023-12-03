2dc1a0b793249b328cdda9d6bd968f008a6f2557
JUan <ladeirajuan96@gmail.com>
2023-12-03 16:28:24 -0300

    atualizando documentação com openapiparameter

--- core/product/views/produto_view.py
+++ core/product/views/produto_view.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -21,16 +21,29 @@ class ProdutoViewSet(viewsets.ViewSet):
             responses=ProdutoSerializer
             )
     def list(self, request):
+        """
+        Recurso para Listar todos os produtos
+        """
         serializer = ProdutoSerializer(self.queryset, many=True)
         return Response(serializer.data)
     
     @extend_schema(
             summary="Listar produtos por categoria",
             description="Retorna uma lista de produtos por categoria",
-            responses=ProdutoSerializer
+            responses=ProdutoSerializer,
+            parameters=[
+                OpenApiParameter(
+                    name='categoria', 
+                    type=OpenApiTypes.STR, 
+                    location=OpenApiParameter.PATH, 
+                    description="Nome da categoria"),
+            ]
             )
     @action(detail=False, methods=['get'], url_path=r"categoria/(?P<categoria>\w+)/all")
     def list_product_by_category(self, request, categoria=None):
+        """
+        Recurso para Listar produtos por categoria
+        """
         serializer = ProdutoSerializer(
             self.queryset.filter(categoria__nome=categoria), many=True
             )
