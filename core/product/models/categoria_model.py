dc61a3f5e27fe76ba13df74934ad23841d65c638
JUan <ladeirajuan96@gmail.com>
2024-01-07 21:13:03 -0300

    definidindo queryset active

--- core/product/models/categoria_model.py
+++ core/product/models/categoria_model.py
@@ -1,6 +1,6 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
-
+from core.product.querysets.active_queryset import ActiveQueryset
 # Create your models here.
 
 
@@ -8,6 +8,10 @@ class Categoria(MPTTModel):
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True, help_text="Categoria pai")
     nome = models.CharField(max_length=80, unique=True, help_text="Nome da categoria")
     slug = models.SlugField(max_length=80, unique=True, help_text="Slug da categoria")
+    
+    is_active = models.BooleanField(default=True, help_text="Categoria ativa")
+
+    objects = ActiveQueryset.as_manager()
 
     class MPPTMeta:
         order_insertion_by = ['nome']
